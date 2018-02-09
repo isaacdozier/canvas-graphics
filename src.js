dcf035a5bfd2d0393ed2ff7d7dc9b90be9b62202
Isaac Dozier <isaactdozier@gmail.com>
2018-02-09 07:50:21 +0000

    v0.0.1-beta: initial

--- src.js
+++ src.js
@@ -3,26 +3,26 @@ var timer = (2/(60))
 var sl = (1.5)-(timer/2), sn = (sl + (timer))
 var ml = (1.5)-(timer/2), mn = (ml + (timer))
 var w = 440, h = 340
-var c = document.getElementById('animations')
+var c = document.getElementById('graphic')
 var ctx = c.getContext("2d") 
 var center = h/2
 var tick = 0
 
 function init(c){
-    document.getElementById('animations').width = w
-    document.getElementById('animations').height = h
+    document.getElementById('graphic').width = w
+    document.getElementById('graphic').height = h
     c.fillRect(0,0,w,h);
-    grid(ctx)
-    xy(ctx)
+    setInterval(function(){
+        grid(c)
+    }, 90);
 }
 
 function go(c){
-    var timeStart = Date.now()
      setInterval(function(){
         build(c,tick)
-        displayTick(c)
-    }, 1000-(Date.now()-timeStart));
+    },4);
 }
+
 
 function build(c,t){
     c.fillStyle = 'black'
@@ -43,69 +43,48 @@ function build(c,t){
     sl = sl + timer
     
     grid(c)
-    xy(c)
 }
 
-function hour(){
-    return Math.floor(tick/60/60)
-    }
-    
-function minute(){  
-    return Math.floor(tick/60) - (hour()*60)
-    }
-    
-function second(){  
-    return tick - (minute()*60) - (hour()*60*60)
-    }
-
-function displayTick(c){
-    var time = leadZero(hour()) + ':' + leadZero(minute()) + ':' + leadZero(second())
-
-    c.beginPath()
-    c.fillStyle = "white"
-    c.font = "28px Arial";
-    c.fillText(time,10,50);
-    c.stroke()
-    
-    tick++
-}
-
-function leadZero(n){if(n < 10){return "0" + n} else {return n}}
-
-//X and Y 0 Coords
-function xy(c){
-    c.strokeStyle= 'white'
-    
-    c.beginPath();
-    c.moveTo(w/2,0);
-    c.lineTo(w/2,h);
-    c.stroke()
-    c.moveTo(0,h/2);
-    c.lineTo(w,h/2);
-    c.stroke()
-}
-
+var mv = 0
 function grid(c){
-    var gh = h/((h)/20)
-    var gw = w/((w)/20)
+    
+    c.beginPath()
+    c.fillRect(0,0,w,h);
+    
+    if(mv === 20){
+        mv = 0
+    }
+    
+    var space = 20
+    var zh = h/4
+    var zw = w/4
+    var gh = (h/(h/space))
+    var gw = (w/(w/space))
     
     c.strokeStyle= 'grey'
     
+    var start = 0
+    
     //horizontal lines
     var hn = 0
-    for(var i = 0; i < (h/(h/gh));i++){
-        ctx.moveTo(0,hn);
-        c.lineTo(w,hn);
-        hn=hn+(h/gh)
+    for(var i = 0; i < h/gh;i++){
+        ctx.moveTo(hn+mv+(w-h),start);
+        c.lineTo(h+(w-h),hn+mv);
+        hn=hn+gh
     }
+    
+  
     
     //vertical lines
     var wn = 0
-    for(var i = 0; i < ((w/gw));i++){
-        ctx.moveTo(wn,0);
-        c.lineTo(wn,h);
-        wn=wn+(gw)
+    for(var i = 0; i < w/gw;i++){
+        ctx.moveTo(start,wn-mv);
+        c.lineTo(wn-mv,h);
+        wn=wn+gw
     }
+
+    mv = mv+4
+    
     
     //draw
     c.stroke()
